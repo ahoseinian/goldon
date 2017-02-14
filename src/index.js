06587d00e7f761cfb6622555f85443904d7ab785
Amir Hoseinian <amir.anyware@gmail.com>
2017-02-14 18:49:08 +0330

    excluding store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
-import createLogger from 'redux-logger'
-import createPromiseMiddleware from 'redux-promise-middleware'
-import goldonApp from './reducers'
 import Router from './Router'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import 'react-select/dist/react-select.css'
 import 'font-awesome/css/font-awesome.css'
 
-const logger = createLogger({collapsed: true});
-const promiseMiddleware = createPromiseMiddleware();
-const store = createStore(goldonApp, applyMiddleware(logger, promiseMiddleware))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {createStore, applyMiddleware} from 'redux'
+import goldonApp from './reducers'
+import createLogger from 'redux-logger'
+import createPromiseMiddleware from 'redux-promise-middleware'
+
+const logger = createLogger({collapsed: true});
+const promiseMiddleware = createPromiseMiddleware();
+const store = createStore(goldonApp, applyMiddleware(logger, promiseMiddleware))
+
+export default store
