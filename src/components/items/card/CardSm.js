e9f63a0910e57597d0d6571c3bab0a74786d4f3e
Amir Hoseinian <amir.anyware@gmail.com>
2017-02-14 22:35:49 +0330

    cardSm size style change

--- src/components/items/card/CardSm.js
+++ src/components/items/card/CardSm.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router'
 import ItemCard from '../../common/Card'
 import ItemImage from '../Image'
 import PriceRow from './PriceRow'
+import Sizes from './Sizes'
 
 const ItemName = styled.div `
   text-align:center;
@@ -16,8 +17,7 @@ const ItemName = styled.div `
 `
 
 const Card = ({item, addItemToCart}) => (
-  <Link to={`/items/${item.id}/${item.name}`}>
-
+  <Link to={`/items/${item.id}/${item.name}`} className="text-muted">
     <ItemCard className="card">
       <div className="row">
         <div className="col-5">
@@ -26,6 +26,7 @@ const Card = ({item, addItemToCart}) => (
         <div className="col-7">
           <ItemName>{item.name}</ItemName>
           <PriceRow price={item.price} />
+          <Sizes size={item.size} />
         </div>
       </div>
     </ItemCard>
--- src/components/items/card/Sizes.js
+++ src/components/items/card/Sizes.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Disc from './Disc'
 
 const Sizes = ({size}) => (
-  <div className="d-flex justify-content-between p-3">
-    <Disc name="طول" value={size.tool} measure="سانتی متر"/>
-    <Disc name="عرض" value={size.arz} measure="سانتی متر"/>
-    <Disc name="ارتفاع" value={size.ertefa} measure="سانتی متر"/>
+  <div className="d-flex pt-2">
+    <small className="ml-2">سایز:</small>
+    <small>{size.tool}</small>
+    <small>*</small>
+    <small>{size.arz}</small>
+    <small>*</small>
+    <small>{size.ertefa}</small>
+    <small>سانتی متر</small>
   </div>
 )
 
