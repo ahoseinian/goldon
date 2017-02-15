ee7cfb9b1a8cde8ab6c539ff3311b51d75aacef9
Amir Hoseinian <amir.anyware@gmail.com>
2017-02-15 13:17:24 +0330

    Sizes Style change

--- src/components/items/card/Sizes.js
+++ src/components/items/card/Sizes.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 const Sizes = ({size}) => (
-  <div className="d-flex pt-2">
-    <small className="ml-2">سایز:</small>
-    <small>{size.tool}</small>
-    <small>*</small>
-    <small>{size.arz}</small>
-    <small>*</small>
-    <small>{size.ertefa}</small>
-    <small className="mr-1">سانتی متر</small>
+  <div className="d-flex justify-content-between pt-1 pl-3">
+    <div>
+      <small className="ml-2">سایز:</small>
+    </div>
+    <div>
+      <small>{size.tool}</small>
+      <small>*</small>
+      <small>{size.arz}</small>
+      <small>*</small>
+      <small>{size.ertefa}</small>
+      <small className="mr-1">سانتی متر</small>
+    </div>
   </div>
 )
 
