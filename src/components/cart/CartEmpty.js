45a4b678008661c0e6a4dccf0cc60bb18cf6d380
Amir Hoseinian <amir.anyware@gmail.com>
2017-02-16 15:48:14 +0330

    show CartIcon When cart's not empty

--- src/components/cart/CartEmpty.js
+++ src/components/cart/CartEmpty.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {Link} from 'react-router'
 
 const CartEmpty = () => (
-  <p className="container text-muted text-center p-3">
+  <div className="container text-muted text-center p-3">
     <div>محصولی در سبد موجود نمیباشد.</div>
     <Link to="/">بازگشت</Link>
-  </p>
+  </div>
 );
 
 
--- src/components/cart/CartIcon.js
+++ src/components/cart/CartIcon.js
@@ -7,15 +7,17 @@ import Icon from '../common/Icon'
 class Cart extends Component {
   render() {
     const {itemsCount} = this.props
-    return (
-      <Link to="/cart">
-        <PrimaryButton>
-          <Icon name="cart-arrow-down"/>
-          <span className="mx-1">سبد</span>
-          <u className="mx-1">{itemsCount}</u>
-        </PrimaryButton>
-      </Link>
-    )
+    return itemsCount
+      ? (
+        <Link to="/cart">
+          <PrimaryButton>
+            <Icon name="cart-arrow-down"/>
+            <span className="mx-1">سبد</span>
+            <u className="mx-1">{itemsCount}</u>
+          </PrimaryButton>
+        </Link>
+      )
+      : null
   }
 }
 export default connect((state) => ({itemsCount: state.cart.items.length}))(Cart)
