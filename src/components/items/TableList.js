2e771c211c6a23d4928a74a754451b50e0b489b9
Amir Hoseinian <amir.anyware@gmail.com>
2017-02-17 22:02:26 +0330

    TableList style change

--- src/components/items/TableList.js
+++ src/components/items/TableList.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Row from './Row'
+import Icon from '../common/Icon'
 
 const TableList = ({items}) => {
 
   return (
     <div className="table-responsive">
       <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th><Icon name="camera"/></th>
+            <th>نام</th>
+            <th>تعداد و قیمت</th>
+            <th><Icon name="edit"/></th>
+          </tr>
+        </thead>
         <tbody>
           {items.map(x => <Row item={x} key={x.id}/>)}
         </tbody>
