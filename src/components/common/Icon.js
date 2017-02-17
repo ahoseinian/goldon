f1102a0b577afcaa609df25cbc0f54bd9797d06b
Amir Hoseinian <amir.anyware@gmail.com>
2017-02-17 21:54:14 +0330

    Icon style change

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,4 +1,4 @@
 import React from 'react'
 
-const IconSpan = ({name}) => <span className={`fa fa-${name}`}></span>
+const IconSpan = ({name}) => <span className={`fa fa-fw fa-${name}`}></span>
 export default IconSpan
