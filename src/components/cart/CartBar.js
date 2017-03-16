316272a11457e36d23c204a9c4d768bd0742796f
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-16 10:33:54 +0330

    Search Link removed

--- src/components/cart/CartBar.js
+++ src/components/cart/CartBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import CartIcon from './CartIcon'
-import SearchButton from '../search/SearchButton'
+// import SearchButton from '../search/SearchButton'
 import HomeLink from '../common/Links/HomeLink'
 
 const CartBar = ({location}) => {
   const TopLink = location.pathname === '/'
-    ? <SearchButton/>
+    ? null
     : <HomeLink/>
+
   return (
     <div className="d-flex justify-content-between py-2">
       {TopLink}
