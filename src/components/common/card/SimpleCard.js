30346578f632449efaf0cd04278957b8212a74fa
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-17 16:46:08 +0330

    LINE break fix

--- src/components/common/card/SimpleCard.js
+++ src/components/common/card/SimpleCard.js
@@ -1,13 +1,18 @@
 import React, {PropTypes} from 'react';
+import styled from 'styled-components'
+
+const TextParagraph = styled.p`
+  white-space: pre-line;
+`
 
 const SimpleCard = ({title, children}) => children
   ? (
     <div className="card my-1">
       <div className="card-header text-center">
-        <h5>{title}</h5>
+        <h5 className="m-2">{title}</h5>
       </div>
       <div className="card-block">
-        <p>{children}</p>
+        <TextParagraph>{children}</TextParagraph>
       </div>
     </div>
   )
