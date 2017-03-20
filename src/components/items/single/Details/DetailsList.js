4aa39df5c5e4b34ec9c3ffb83e9179752b6d0b25
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-20 12:19:53 +0330

    Details style change

--- src/components/items/single/Details/DetailsList.js
+++ src/components/items/single/Details/DetailsList.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import Card from '../../../common/card/Card'
 import SingleRowTable from '../../../common/SingleRowTable'
 import LinkList from '../../../common/Links/LinkList'
 
 const DetailsList = ({item}) => (
-  <Card className="card-block">
+  <div>
     <SingleRowTable name="مدل">{item.modelsNames.join(' , ')}</SingleRowTable>
     <SingleRowTable name="رنگهای موجود">{item.colors.join(' , ')}</SingleRowTable>
     <SingleRowTable name="نام های دیگر">{item.names.join(' , ')}</SingleRowTable>
     <SingleRowTable name="لینک ها">
       {item.links.length && <LinkList links={item.links}/>}
     </SingleRowTable>
-  </Card>
+  </div>
 );
 
 DetailsList.propTypes = {
--- src/components/items/single/Details/index.js
+++ src/components/items/single/Details/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import SimpleCard from '../../../common/card/SimpleCard'
 import DetailsList from './DetailsList'
 import SizeList from './SizeList'
+import Card from '../../../common/card/Card'
 
 const Details = ({item}) => (
   <div>
-    <SizeList {...item.size}/>
-    <DetailsList item={item}/>
+    <Card className="card-block">
+      <SizeList {...item.size}/>
+      <DetailsList item={item}/>
+    </Card>
     <SimpleCard title="نگهداری">{item.maintenance}</SimpleCard>
     <SimpleCard title="توضیحات">{item.information}</SimpleCard>
     <SimpleCard title="خواص">{item.benefits}</SimpleCard>
