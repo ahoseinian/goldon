82e900d24181fc7708508aafcd687a8d54a70867
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-27 17:06:47 +0430

    PageHead test added

---
+++ src/components/PageHead.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import PageHead from './PageHead'
+
+describe('PageHead', () => {
+  it('renders a Title', () => {
+    const wrapper = shallow(<PageHead />)
+  })
+})
