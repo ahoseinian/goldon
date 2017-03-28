c0a15c70eed7e46bfafffebbc1f95a0d6a0502b4
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-28 11:12:51 +0430

    CartEmpty test

---
+++ src/components/cart/CartEmpty.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import CartEmpty from './CartEmpty'
+
+describe('CartEmpty', () => {
+  it('renders', () => {
+    const wrapper = shallow(<CartEmpty />)
+  })
+})
