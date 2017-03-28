e088979f3d2f4452d54c1199807437856162713f
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-28 19:20:54 +0430

    Small component tests added

---
+++ src/components/common/typography/Small.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {Small} from './Small'
+
+describe('Small', () => {
+  it('renders', () => {
+    const wrapper = shallow(<Small />)
+  })
+})
