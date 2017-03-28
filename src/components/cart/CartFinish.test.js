18d6463c987c84f58b5bb2f83150cd0117550ee6
Amir Hoseinian <amir.anyware@gmail.com>
2017-03-28 11:20:32 +0430

    CartFinish test

---
+++ src/components/cart/CartFinish.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import CartFinish from './CartFinish'
+
+describe('CartFinish', () => {
+  it('renders', () => {
+    const wrapper = shallow(<CartFinish />)
+  })
+})
