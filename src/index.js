6eba757fc0c855e57effb38b6dc4faa1a37b56e4
Amir Hoseinian <amir.anyware@gmail.com>
2017-04-02 00:58:38 +0430

    Fix import path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import Router from './Router'
+import Router from './containers/Router'
 import store from './store'
 import {getAllItems} from './actions/index'
 import 'bootstrap/dist/css/bootstrap.css'
