4216e15f7910f31e0a2376db216f52fd9e7c5b7a
Amir Hoseinian <amir.anyware@gmail.com>
2017-04-01 18:02:42 +0430

    Add Home title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import {Element} from 'react-scroll'
+import {Helmet} from 'react-helmet'
 import {getSearchedItems} from '../reducers/search'
 import ItemList from './items/list'
 import PageHead from './PageHead'
-import {Element} from 'react-scroll'
 
 export const Home = ({items}) => (
   <div>
+    <Helmet>
+      <title>میاریم</title>
+    </Helmet>
     <PageHead/>
     <div className="container">
       <Element name="firstInsideContainer">
