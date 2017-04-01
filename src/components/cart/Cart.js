2388927299ecda76d2dfb71859073d24a368b74f
Amir Hoseinian <amir.anyware@gmail.com>
2017-04-01 14:54:20 +0430

    Add Cart handleSubmit() tests

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -9,55 +9,49 @@ import CartForm from './CartForm'
 import CartEmpty from './CartEmpty'
 import SingleRowTable from '../common/SingleRowTable'
 
-export const Cart = ({cart, fullPrice, completeCartOrder}) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const {
-      target: {
-        elements: {
-          tel: {
-            value: tel
-          },
-          address: {
-            value: address
-          },
-          fullname: {
-            value: fullname
-          }
-        }
-      }
-    } = e;
-    const items = cart
-      .items
-      .map(({id, quantity}) => ({item: id, quantity}))
-    completeCartOrder({fullname, tel, address, items}).then(() => browserHistory.push('/cart/finish'))
+export class Cart extends React.Component {
+  static propTypes = {
+    cart: React.PropTypes.shape({
+      items: React.PropTypes.arrayOf(React.PropTypes.object)
+    }).isRequired,
+    fullPrice: React.PropTypes.number.isRequired
   }
 
-  return (
-    <div>
-      <h1 className="text-center py-3 my-3">سبد خرید</h1>
-      {cart.items.length
-        ? <div className="container">
-            <CartTable items={cart.items}/>
-            <SingleRowTable name="مجموع">
-              <Price value={fullPrice}/>
-            </SingleRowTable>
-            <CartForm submit={handleSubmit} disabled={!cart.items.length}/>
-          </div>
-        : <CartEmpty/>}
+  construct(){
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-    </div>
-  )
-}
+  handleSubmit(e) {
+    e.preventDefault();
+    const tel = e.target.elements.tel.value
+    const address = e.target.elements.address.value
+    const fullname = e.target.elements.fullname.value
 
-Cart.propTypes = {
-  cart: React.PropTypes.shape({
-    items: React.PropTypes.arrayOf(
-      React.PropTypes.object
+    const items = this.props.cart.items.map(({id, quantity}) => ({item: id, quantity}))
+    this.props.completeCartOrder({fullname, tel, address, items})
+      .then(() => browserHistory.push('/cart/finish'))
+  }
+
+  render() {
+    const {cart, fullPrice} = this.props
+    return (
+      <div>
+        <h1 className="text-center py-3 my-3">سبد خرید</h1>
+        {cart.items.length
+          ? <div className="container">
+              <CartTable items={cart.items}/>
+              <SingleRowTable name="مجموع">
+                <Price value={fullPrice}/>
+              </SingleRowTable>
+              <CartForm submit={this.handleSubmit} disabled={!cart.items.length}/>
+            </div>
+          : <CartEmpty/>}
+
+      </div>
     )
-  }).isRequired,
-  fullPrice: React.PropTypes.number.isRequired
+  }
 }
+
 
 export default connect((state) => ({
   cart: state.cart,
--- src/components/cart/Cart.test.js
+++ src/components/cart/Cart.test.js
@@ -30,4 +30,37 @@ describe('Cart', () => {
     expect(wrapper.find(CartTable).first().props().items).toEqual(items)
   })
 
+  describe('handleSubmit()', () => {
+    it('calls completeCartOrder with right arguments', () => {
+      const thenMock = jest.fn()
+      const completeCartOrder = jest.fn().mockImplementation(() => ({then: thenMock}));
+
+      const wrapper = shallow(<Cart
+        cart={{items:[]}}
+        fullPrice={10}
+        completeCartOrder={completeCartOrder}/>)
+
+      const event = {
+        preventDefault: jest.fn(),
+        target:{
+          elements:{
+            tel: {value: 'tel'},
+            address: {value: 'add'},
+            fullname: {value: 'name'},
+          }
+        }
+      }
+
+      wrapper.instance().handleSubmit(event)
+      expect(completeCartOrder.mock.calls.length).toEqual(1)
+      expect(thenMock.mock.calls.length).toEqual(1)
+      expect(completeCartOrder.mock.calls[0][0]).toEqual({
+        tel: 'tel',
+        address: 'add',
+        fullname: 'name',
+        items: []
+      })
+
+    })
+  })
 })
