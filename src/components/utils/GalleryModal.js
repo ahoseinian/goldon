3ed62a3d023b24dcedca14c5ceb64f4fae6a48c0
Amir Hoseinian <amir.anyware@gmail.com>
2017-04-06 01:18:07 +0430

    Add mapStateToProps to GalleryModal

--- src/components/utils/GalleryModal.js
+++ src/components/utils/GalleryModal.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Modal from 'react-modal'
+import {connect} from 'react-redux'
+import {closeGallery} from '../../actions'
 
-export const GalleryModal = ({isOpen, src}) => (
-  <Modal contentLabel="Gallery" isOpen={isOpen}>
-
+export const GalleryModal = ({isOpen = false, src, closeGallery}) => (
+  <Modal contentLabel="Gallery" isOpen={isOpen} onRequestClose={closeGallery}>
+    <img src={src} className="gallery-image"/>
   </Modal>
 )
 
 GalleryModal.propTypes = {
   src: React.PropTypes.string.isRequired,
-  isOpen: React.PropTypes.bool.isRequired
+  isOpen: React.PropTypes.bool,
+  closeGallery: React.PropTypes.func.isRequired
 }
 
-export default GalleryModal
+export const mapStateToProps = ({gallery}) => ({
+  src: `/images/original/${gallery.id}.jpg`,
+  isOpen: gallery.isOpen
+})
+
+export default connect(mapStateToProps, {closeGallery})(GalleryModal)
--- src/components/utils/GalleryModal.test.js
+++ src/components/utils/GalleryModal.test.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import Modal from 'react-modal'
-import {GalleryModal} from './GalleryModal'
+import {GalleryModal, mapStateToProps} from './GalleryModal'
 
 describe('GalleryModal', () => {
   it('renders', () => {
-    const wrapper = shallow(<GalleryModal src="test" isOpen/>)
+    const wrapper = shallow(<GalleryModal src="test" isOpen closeGallery={jest.fn()}/>)
   })
 
   it('has react-modal in it', () => {
-    const wrapper = shallow(<GalleryModal src="test" isOpen/>)
+    const wrapper = shallow(<GalleryModal src="test" isOpen closeGallery={jest.fn()}/>)
     const modal = wrapper.find(Modal)
 
     expect(
@@ -20,4 +20,29 @@ describe('GalleryModal', () => {
       modal.props().isOpen
     ).toEqual(true)
   })
+
+  it('has a image with source on it', () => {
+    const wrapper = shallow(<GalleryModal src="test" isOpen closeGallery={jest.fn()}/>)
+    expect(wrapper.find('.gallery-image').length).toEqual(1)
+  })
+
+  it('sends close gallery prop to react-modal', () => {
+    const closeGalleryFunc = jest.fn()
+    const wrapper = shallow(<GalleryModal src="test" isOpen closeGallery={closeGalleryFunc}/>)
+    const modal = wrapper.find(Modal)
+    expect(modal.props().onRequestClose).toEqual(closeGalleryFunc)
+  })
+})
+
+describe('mapStateToProps', () => {
+  it('has src and isOpen props', () => {
+    const state = {
+      gallery: { id: 'test', isOpen: true }
+    }
+    expect(mapStateToProps(state)).toEqual({
+      src:'/images/original/' + state.gallery.id + '.jpg',
+      isOpen: true
+    })
+  })
+
 })
