4c936454050c7cd51b9b9067f1d32b5df88e4a91
Amir Hoseinian <amir.anyware@gmail.com>
2017-04-06 01:48:03 +0430

    GalleryModal added

--- src/components/utils/GalleryModal.js
+++ src/components/utils/GalleryModal.js
@@ -3,9 +3,27 @@ import Modal from 'react-modal'
 import {connect} from 'react-redux'
 import {closeGallery} from '../../actions'
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    maxWidth              : '95%',
+    maxHeight             : '85%'
+  }
+}
+
 export const GalleryModal = ({isOpen = false, src, closeGallery}) => (
-  <Modal contentLabel="Gallery" isOpen={isOpen} onRequestClose={closeGallery}>
-    <img src={src} className="gallery-image"/>
+  <Modal
+    contentLabel="Gallery"
+    isOpen={isOpen}
+    onRequestClose={closeGallery}
+    style={customStyles}
+  >
+    <img src={src} className="gallery-image" alt=""/>
   </Modal>
 )
 
